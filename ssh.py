046d75784bf18f5fc97dd816beda29ece4316ae3
Jordan Chen <jordan.chen@moxa.com>
2014-09-29 17:18:50 +0800

    add get function in ssh.py and add modify the response code in test_ssh.py to make the coverage to 100%

--- ssh.py
+++ ssh.py
@@ -6,16 +6,20 @@ from sanji.core import Sanji
 from sanji.core import Route
 from sanji.connection.mqtt import Mqtt
 
+logger = logging.getLogger()
 
 class Ssh(Sanji):
 
     def init(self):
-        self.message = "Hello Sanji!"
+        self.message = "Hello SSH!"
 
     @Route(methods="get", resource="/network/ssh")
     def get(self, message, response):
+        logger.debug("in get function !!")
+        #self.get_row()
         response(data={"message": self.message})
 
+    '''
     @Route(methods="put", resource="/network/ssh")
     def put(self, message, response):
         if hasattr(message, "data"):
@@ -38,7 +42,7 @@ class Ssh(Sanji):
                 return response()
 
         return response(code=400, data={"message": "Invalid Delete Input."})
-
+    '''
 
 if __name__ == '__main__':
     FORMAT = '%(asctime)s - %(levelname)s - %(lineno)s - %(message)s'
