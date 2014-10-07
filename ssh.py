e55dcc27b55222c7609bfcb547ebbde25038cb1c
Jordan Chen <jordan.chen@moxa.com>
2014-10-07 14:31:58 +0800

    add put function in ssh model

--- ssh.py
+++ ssh.py
@@ -4,6 +4,7 @@
 import logging
 import os
 import subprocess
+import uuid
 from sanji.core import Sanji
 from sanji.core import Route
 from sanji.model_initiator import ModelInitiator
@@ -22,44 +23,24 @@ class Ssh(Sanji):
 
     @Route(methods="get", resource="/network/ssh")
     def get(self, message, response):
-        response(data={"enable": self.model.db["enable"]})
+        return response(data={"enable": self.model.db["enable"]})
 
-    '''
     @Route(methods="put", resource="/network/ssh")
     def put(self, message, response):
         if hasattr(message, "data"):
-            self.message = message.data["enable"]
-            logger.debug("in put function")
-            logger.debug(" put_data:%s" % db_data["enable"])
-            # insert data to db
-            # if update db success
-                # if enable = 1
-                    # restart ssh
-                     #if restart success
-                        #return 200
-                # else
-                    # stop ssh
-                        # if stop db success
-            # else
-                # (print log message)update db failed            
-            return response()
-        return response(code=400, data={"message": "Invaild Input."})
-    '''
-    # def recover_db_from_factory(self):
-    #     cmd = "cp %s %s" % (self.model_profile["model_factory_db"],
-    #           self.model_profile["model_db"])
-    #     subprocess.call(cmd, shell=True)
+            enable = message.data["enable"]
+            self.model.db["enable"] = message.data["enable"]
+            self.model.save_db()
+            self.update_ssh(enable, message, response)
 
     def start_model(self):
-        cmd = "service ssh start"
+        cmd = "service ssh restart"
         subprocess.call(cmd, shell=True)
         rc = self.check_ssh()
         if rc is True:
             logger.info("ssh daemon start successfully.")
-            print "ssh start success"
         else:
             logger.info("ssh daemon start failed.")
-            print "ssh start failed"
 
     def check_ssh(self):
         find_process = None
@@ -67,14 +48,42 @@ class Ssh(Sanji):
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                    shell=True)
         grep_rc = process.communicate()[0]
-        print("grep_rc:%s" % grep_rc)
         find_process = grep_rc.find("/usr/sbin/sshd")
-        print("find_process:%s" % find_process)
         if find_process != -1:
             return True
         else:
-            print "-------------> grep failed"
             return False
+
+    def start_ssh(self, message, response):
+        cmd = "service ssh restart"
+        subprocess.call(cmd, shell=True)
+        rc = self.check_ssh()
+        if rc is True:
+            logger.info("ssh daemon start successfully.")
+            return response(data=message.data)
+        else:
+            logger.info("ssh daemon start failed.")
+            return response(code=400,
+                            data={"message": "ssh daemon start failed"})
+
+    def stop_ssh(self, message, response):
+        cmd = "service ssh stop"
+        subprocess.call(cmd, shell=True)
+        rc = self.check_ssh()
+        if rc is False:
+            logger.info("ssh daemon stop successfully.")
+            return response(data=message.data)
+        else:
+            logger.info("ssh daemon stop failed.")
+            return response(code=400,
+                            data={"message": "ssh daemon stop failed"})
+
+    def update_ssh(self, enable, message, response):
+        if enable == 1:
+            self.start_ssh(message, response)
+        else:
+            self.stop_ssh(message, response)
+
 
 if __name__ == '__main__':
     FORMAT = '%(asctime)s - %(levelname)s - %(lineno)s - %(message)s'
